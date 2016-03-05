e6731053b306b68b748a08c102d9875dcdfc1cb1
Abby Yeh <radleaf@gmail.com>
2016-03-05 21:08:39 +0800

    work; no level

--- Azalea/web/dart/main.dart
+++ Azalea/web/dart/main.dart
@@ -1,16 +1,17 @@
 import 'dart:html';
-import 'dart:math';
 import 'dart:async';
 
-import 'dart:js' as js;
-
 //elements
+Element gameCanvas;
 Element target;
 Element scoreBoard;
 Element startBtn;
-Element timdDisplay;
+Element timeDisplay;
 //score
 int cnt;
+int cntForAnimate;
+int level;
+bool going;
 //timer
 RequestAnimationFrameCallback _callback;
 num _whenStarted;
@@ -19,46 +20,79 @@ num _whenStarted;
 void main() {
 	initGame();
 	initEvent();
-
-
 }
 
 void initGame() {
+  gameCanvas = querySelector('.game-canvas');
 	target = querySelector('.game-canvas img');
 	scoreBoard = querySelector('.score-band');
 	startBtn = querySelector('.start-button');
-	timdDisplay = querySelector('.time-display');
+	timeDisplay = querySelector('.time-dispaly');
 	cnt = 0;
+  cntForAnimate = 0;
+  level = 1;
+  going = false;
 
-    _callback = (num now) {
-      //get now
-      if (_whenStarted == null) {
-        _whenStarted = now;
-      }
-      now -= _whenStarted;
+  _callback = (num now) {
+    //get now
+    if (_whenStarted == null) {
+      _whenStarted = now;
+    }
+    now -= _whenStarted;
+    cntForAnimate++;
+
+    if (cntForAnimate == 5) {
+      cntForAnimate = 0;
       //update time
-      if (now % 100 == 0) {
-
-      }
-      //update picture
-
+      if ((now~/100)%10 == 0)
+        timeDisplay.text = "${10-(now~/1000)}.0s";
+      else
+        timeDisplay.text = "${9-(now~/1000)}.${10-((now~/100)%10)}s";
+      // //update picture
+      // if (level < 10 && cnt > levelUp()) {
+      //   gameCanvas.querySelector(".gh-$level").classes.toggle("hidden");
+      //   gameCanvas.querySelector(".gh-${level+1}").classes.toggle("hidden");
+      //   level += 1;
+      //   print(level);
+      // }
       //update score
+      scoreBoard.text = "$cnt";
+    }
 
-      //request again
+    //request again
+    if (now / 1000 >= 10) {
+      timeDisplay.text = "0.0s";
+      going = false;
+      showResult();
+    } else {
       window.requestAnimationFrame(_callback);
-    };
-  }
+    }
+  };
+}
 
 void initEvent() {
 	startBtn.onMouseUp.listen((MouseEvent) {
-		//start requestAnimationFrame
-		//hide start and display time
-		startBtn.classes.add("hidden");
-		timdDisplay.classes.remove("hidden");
+    going = true;
+    new Timer(const Duration(milliseconds:100), () {
+  		//start requestAnimationFrame
+      _whenStarted = null;
+      window.requestAnimationFrame(_callback);
+  		//hide start button and display time
+  		startBtn.classes.add("hidden");
+  		timeDisplay.classes.toggle("hidden");
+    });
 	});
+
+  target.onMouseUp.listen((MouseEvent) {
+    if (going) {
+      cnt++;
+    }
+  });
 }
 
-void show() {
+void showResult() {
 
 }
+
+int levelUp() => level*level + 5;
 
